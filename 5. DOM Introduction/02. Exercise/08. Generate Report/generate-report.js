74f7bc3dbdfc420c035f48b6ccdc85ab993e019a
AStoyanoff <chessterfield@gmail.com>
2025-11-12 18:09:29 +0200

    Add changes

--- 5. DOM Introduction/02. Exercise/08. Generate Report/generate-report.js
+++ 5. DOM Introduction/02. Exercise/08. Generate Report/generate-report.js
@@ -1,3 +1,29 @@
 function solve() {
-    //TODO
+    
+    let checkboxes = document.querySelectorAll('th input[type="checkbox"]');
+    let columns = [];
+
+    for (let pos = 0; pos < checkboxes.length; pos++) {
+        if (checkboxes[pos].checked) columns.push(pos);
+    }
+   
+    let rows = document.querySelectorAll('tbody tr');
+    let result = [];
+
+   
+    for (let i = 0; i < rows.length; i++) {
+
+        let cells = rows[i].querySelectorAll('td');
+        let object = {};
+       
+        for (let idx = 0; idx < columns.length; idx++) {
+
+            let colIdx = columns[idx];
+            let name = checkboxes[colIdx].name;
+            object[name] = cells[colIdx].textContent;
+        }
+
+        result.push(object);
+    }
+    document.getElementById('output').value = JSON.stringify(result);
 }
--- 5. DOM Introduction/02. Exercise/09. Number Convertor/number-convertor.js
+++ 5. DOM Introduction/02. Exercise/09. Number Convertor/number-convertor.js
@@ -1,3 +1,15 @@
 function solve() {
-    // TODO
+    
+    const number = document.getElementById('input');
+    const selectElement = document.getElementById('selectMenuTo');
+    const result = document.getElementById('result');
+
+    const num = Number(number.value.trim());
+    const unitTo = selectElement.value;
+
+    if (unitTo === 'binary')
+        result.value += num.toString(2);
+
+    else if (unitTo === 'hexadecimal')
+        result.value += num.toString(16).toLocaleUpperCase();
 }
