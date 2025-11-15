98fb8bf1d5bdf7c012d3cfcf07fd2bd5c9f3a445
AStoyanoff <chessterfield@gmail.com>
2025-11-15 22:49:35 +0200

    Make a changes 08.js

--- 005. DOM Introduction/02. Exercise/08. Generate Report/generate-report.js
+++ 005. DOM Introduction/02. Exercise/08. Generate Report/generate-report.js
@@ -1,10 +1,10 @@
 function solve() {
     
     let checkboxes = document.querySelectorAll('th input[type="checkbox"]');
-    let columns = [];
+    let cols = [];
 
     for (let pos = 0; pos < checkboxes.length; pos++) {
-        if (checkboxes[pos].checked) columns.push(pos);
+        if (checkboxes[pos].checked) cols.push(pos);
     }
    
     let rows = document.querySelectorAll('tbody tr');
@@ -16,9 +16,9 @@ function solve() {
         let cells = rows[i].querySelectorAll('td');
         let object = {};
        
-        for (let idx = 0; idx < columns.length; idx++) {
+        for (let idx = 0; idx < cols.length; idx++) {
 
-            let colIdx = columns[idx];
+            let colIdx = cols[idx];
             let name = checkboxes[colIdx].name;
             object[name] = cells[colIdx].textContent;
         }
