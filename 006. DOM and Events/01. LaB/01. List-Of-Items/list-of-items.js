fde7bf6ed5b495a6a0fbd9cf2f6a8d4e77b34b1f
AStoyanoff <chessterfield@gmail.com>
2025-11-16 02:15:43 +0200

    Add 006. DOM and Events

--- 006. DOM and Events/01. LaB/01. List-Of-Items/list-of-items.js
+++ 006. DOM and Events/01. LaB/01. List-Of-Items/list-of-items.js
@@ -1,3 +1,25 @@
 function addItem() {
-    //TODO
+    
+    const getItems = document.getElementById('items');
+    const newItems = document.getElementById('newItemText');
+
+    const currItems = newItems.value.trim(); 
+    const newLiItems = document.createElement('li'); 
+
+    newLiItems.textContent = currItems;
+    getItems.append(newLiItems); 
 }
+
+/**
+ *  1. Взимаме всички 'li' elements по id 
+ *  2. Взимаме новите 'li' elements по id
+ *  -----------------------------------------------------------------------------
+ *  3. Към текущите 'li' добавяме стойността на новите, и премахваме интервалите
+ *  4. Създаваме нов елемент към DOM с таг -> (li) в края на "колекцията"
+ *  -----------------------------------------------------------------------------
+ *  5. Съдържанието на новият 'li' елемент става текущият 'li' елеменет
+ *  6. Взимаме всички 'li' елементи и добавяме към тях новият 'li' елемент 
+ *  -----------------------------------------------------------------------------
+ *  7. crеateElement() -> създава НОВ елемент в DOM дървото 
+ *  8. append() или appendChild() -> добавят елемент в края на колекцията
+ */
