1b6f2a8f3a9c6eb67701bf05ca1303bf94f7bd46
AStoyanoff <chessterfield@gmail.com>
2025-11-23 21:36:29 +0200

    Added new solution

--- 007. Asynchronous Programming/04.Bus-Stop/app.js
+++ 007. Asynchronous Programming/04.Bus-Stop/app.js
@@ -1,3 +1,75 @@
 function getInfo() {
-    console.log("TODO...");
+
+    const stopIdInput = document.getElementById('stopId');
+    const stopNameDiv = document.getElementById('stopName');
+    const busesUl = document.getElementById('buses');
+
+    const stopId = stopIdInput.value;
+
+    stopNameDiv.textContent = '';
+    busesUl.innerHTML = '';
+
+    const URL = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    fetch(URL)
+        .then(res => {
+
+            if (!res.ok) {
+                throw new Error();
+            }
+            return res.json();
+        })
+        .then(data => {
+            stopNameDiv.textContent = data.name;
+
+            for (let busId in data.buses) {
+
+                const li = document.createElement('li');
+                li.textContent = `Bus ${busId} arrives in ${data.buses[busId]} minutes`;
+                busesUl.appendChild(li);
+            }
+        })
+        .catch(() => {
+            stopNameDiv.textContent = 'Error';
+        });
+}
+
+                    //---------------------------------
+                    //             VARIANT 2 - async/await
+                    //---------------------------------
+
+async function getInfo() {
+
+    const stopIdInput = document.getElementById('stopId');
+    const stopNameDiv = document.getElementById('stopName');
+    const busesUl = document.getElementById('buses');
+
+    const stopId = stopIdInput.value;
+    
+    stopNameDiv.textContent = '';
+    busesUl.innerHTML = '';
+
+    const URL = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    try {
+
+        const response = await fetch(URL);
+
+        if (!response.ok) {
+            throw new Error('Invalid request');
+        }
+
+        const data = await response.json();
+        stopNameDiv.textContent = data.name;
+        
+        for (let busId in data.buses) {
+
+            const li = document.createElement('li');
+            li.textContent = `Bus ${busId} arrives in ${data.buses[busId]} minutes`;
+            busesUl.appendChild(li);
+        }
+
+    } catch (error) {
+        stopNameDiv.textContent = 'Error';
+    }
 }
