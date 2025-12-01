e39153817aa76b56b04028ad49467d0aaca6fac3
AStoyanoff <chessterfield@gmail.com>
2025-12-01 17:25:22 +0200

    Added new solve

--- 007. Asynchronous Programming/03. Github-Commits/app.js
+++ 007. Asynchronous Programming/03. Github-Commits/app.js
@@ -1,8 +1,32 @@
-function loadCommits() {
-    
-    const userInput = document.getElementById('username');
-    const repoInput = document.getElementById('repo');
-    const ulInput = document.getElementById('commits');
+async function loadCommits() {
+    // Try it with Fetch API
 
-    
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+    const ul = document.getElementById('commits');
+
+    ul.innerHTML = '';
+    const URL = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    try {
+        const res = await fetch(URL);
+
+        if (!res.ok) {
+            throw new Error(res.status);
+        }
+
+        const data = await res.json();
+
+        data.forEach(e => {
+
+            const li = document.createElement('li');
+            li.textContent = `${e.commit.author.name}: ${e.commit.message}`;
+            ul.appendChild(li);
+        })
+    } catch (err) {
+
+        const li = document.createElement('li');
+        li.textContent = `Error: ${err.message} (Not Found)`;
+        ul.appendChild(li);
+    }
 }
